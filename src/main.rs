d5e5e16cb29a25ce2e6bc6ea33438fac7fb01ed9
rey256 <medvedosandr@gmail.com>
2024-01-03 21:07:51 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
-// #![no_std]
-// #![no_main]
+#![no_std]
+#![no_main]
 
-// use panic_halt as _;
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_stm32::Config;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
-// use cortex_m_rt::entry;
-// use embassy::executor::{task, Executor};
-// use embassy::time::{Duration, Timer};
-// use rtt_target::{rprintln, rtt_init_print};
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let config = Config::default();
+    let _p = embassy_stm32::init(config);
 
-// #[entry]
-// fn main() -> ! {
-//     rtt_init_print!();
-
-//     rprintln!("Hello, world!");
-
-//     let a = 2 + 5;
-
-//     rprintln!("{}", a);
-
-//     loop {
-//         // your code goes here
-//     }
-// }
+    loop {
+        info!("Hello World!");
+        Timer::after_secs(1).await;
+    }
+}
 
 // #[task]
 // async fn tick_periodic() -> ! {
@@ -31,29 +26,27 @@
 //     }
 // }
 
-#![no_std]
-#![no_main]
+// #![no_std]
+// #![no_main]
 // #![feature(type_alias_impl_trait)]
 
-use cortex_m_rt::entry;
 // use embassy_executor::Spawner;
 // use embassy_stm32::exti::ExtiInput;
 // use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
-use {defmt_rtt as _, panic_probe as _};
+// use {defmt_rtt as _, panic_probe as _};
 
 // #[embassy_executor::main]
-#[entry]
-fn main() -> ! {
-    let p = embassy_stm32::init(Default::default());
-    // let mut led = Output::new(p.PB14, Level::Low, Speed::VeryHigh);
-    // let mut button = ExtiInput::new(Input::new(p.PC13, Pull::Up), p.EXTI13);
+// async fn main(_spawner: Spawner) {
+//     let p = embassy_stm32::init(Default::default());
+//     let mut led = Output::new(p.PB14, Level::Low, Speed::VeryHigh);
+//     let mut button = ExtiInput::new(Input::new(p.PC13, Pull::Up), p.EXTI13);
 
-    loop {
-        // button.wait_for_any_edge().await;
-        // if button.is_low() {
-        //     led.set_high();
-        // } else {
-        //     led.set_low();
-        // }
-    }
-}
+//     loop {
+//         button.wait_for_any_edge().await;
+//         if button.is_low() {
+//             led.set_high();
+//         } else {
+//             led.set_low();
+//         }
+//     }
+// }
